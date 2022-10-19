13c98a8b912f8b0c2f7df7e5370fe8c5dc606447
Joseph <joegfay@gmail.com>
2022-10-19 14:54:11 -0500

    Added documentation and condensed into functions.

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,49 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+
+# Remove this eventually replace with try/catch
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
-# html = BeautifulSoup(page.content, "html.parser")
-
-# TODO Write a program that can retrieve and display a site map of any website.
-
-# TODO Retrieve URL from user input
-
 
 def input_url() -> str:
+    """
+    Takes user input in the form of a website URL.
+
+    :return: str
+    """
     return input("[+] Enter a URL: ")
 
 
 def get_robots(url: str) -> requests.Response:
+    """
+    Function used to gain access to site map indexes.
+
+    :param url:
+    :return: URL with concatenated robots.txt route.
+    """
     robots = requests.get("https://" + url + "/robots.txt")
     return robots
 
 
-def get_sitemap(index: str):
+def print_sitemap(index: str):
+    """
+    Output found sitemap XML files to the terminal.
+
+    :param index:
+    """
     url = requests.get(index)
     soup = BeautifulSoup(url.content, "html.parser")
-    p = re.compile(('[h].+[xml]'))
+    pattern = re.compile('[h].+[xml]')
 
-    match_list = re.findall(p, soup.text)
+    match_list = re.findall(pattern, soup.text)
     print("[LOG] Displaying route pages:")
     [print(match) for match in match_list]
 
 
-if __name__ == '__main__':
+def get_index() -> str:
+    """
+    Function used to access a sites map index.
+
+    :return: sitemap_index
+    """
     html = BeautifulSoup(get_robots(input_url()).content, "html.parser")
     p = re.compile('[h].+[xml]$')
     target = re.search(p, html.text)
     print("[LOG] Sitemap index found: ")
     print(target.group())
+    return target.group()
 
-    get_sitemap(target.group())
-    # prize_list = html.find_all(class_="prizes-content")
-    # for prize in prize_list:
-    #     title = prize.find("h6").text.strip()
-    #     description = prize.find('p').text.strip()
-    #     print(f"{title} : {description}")
-    #     ipad = re.search('iPad', description)
+
+def webmap():
+    """
+    Begins the webmap utility
+    """
+    print_sitemap(get_index())
+
+
+if __name__ == '__main__':
+    webmap()
+
 
 
 
