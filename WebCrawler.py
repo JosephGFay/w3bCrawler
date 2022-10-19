68877f3515bb0fe4fd031673381d4d99ec8b944d
Joseph <joegfay@gmail.com>
2022-10-19 13:45:15 -0500

    Functionality for site mapping

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,12 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
 
-# Establish Connection to URL
-URL = "https://icl.cyberbit.com/"
-page = requests.get(URL)
-# Store URL Content into soup object.
-html = BeautifulSoup(page.content, "html.parser")
+# html = BeautifulSoup(page.content, "html.parser")
 
 # TODO Write a program that can retrieve and display a site map of any website.
 
@@ -17,13 +15,29 @@ def input_url() -> str:
     return input("[+] Enter a URL: ")
 
 
-def get_sitemap(url):
-    sitemap = requests.get("https://" + url + "/robots.txt")
-    return sitemap
+def get_robots(url: str) -> requests.Response:
+    robots = requests.get("https://" + url + "/robots.txt")
+    return robots
+
+
+def get_sitemap(index: str):
+    url = requests.get(index)
+    soup = BeautifulSoup(url.content, "html.parser")
+    p = re.compile(('[h].+[xml]'))
+
+    match_list = re.findall(p, soup.text)
+    print("[LOG] Displaying route pages:")
+    [print(match) for match in match_list]
 
 
 if __name__ == '__main__':
-    print(get_sitemap(input_url()))
+    html = BeautifulSoup(get_robots(input_url()).content, "html.parser")
+    p = re.compile('[h].+[xml]$')
+    target = re.search(p, html.text)
+    print("[LOG] Sitemap index found: ")
+    print(target.group())
+
+    get_sitemap(target.group())
     # prize_list = html.find_all(class_="prizes-content")
     # for prize in prize_list:
     #     title = prize.find("h6").text.strip()
